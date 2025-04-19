3a724fd8938b03e0fc69490bc40b167e9f5bba44
SerbynS <serbin.sasha1@gmail.com>
2025-04-19 19:54:28 +0300

    change ListItem clsx

--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -1,11 +1,12 @@
 import css from "./FriendListItem.module.css";
+import clsx from "clsx";
 
 export default function FriendListItem({ info: { avatar, name, isOnline } }) {
   return (
     <div className={css.content}>
       <img src={avatar} alt={name} width={120} />
       <p>{name}</p>
-      <p style={{ color: isOnline ? "green" : "red" }}>
+      <p className={clsx(isOnline ? css.isOnline : css.isOffline)}>
         {isOnline ? "Online" : "Offline"}
       </p>
     </div>
